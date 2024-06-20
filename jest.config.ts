3b99818587962a5656916658475d419d732c18ce
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-06-20 15:12:46 -0600

    chore: initial state

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  projects: [
+    {
+      displayName: 'Unit',
+      moduleDirectories: ['node_modules', __dirname],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+      preset: 'ts-jest',
+      rootDir: 'src',
+      setupFilesAfterEnv: ['dotenv/config'],
+      testEnvironment: 'node',
+      testRegex: '\\.spec\\.ts$',
+    },
+    {
+      displayName: 'E2E',
+      moduleDirectories: ['node_modules', __dirname],
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+      preset: 'ts-jest',
+      rootDir: 'test',
+      setupFilesAfterEnv: ['dotenv/config'],
+      testEnvironment: 'node',
+      testRegex: '\\.e2e.spec\\.ts$',
+    },
+  ],
+};
