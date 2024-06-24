3cdd363812a2f8ec2efe2e8014cf720a14bcc182
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-06-24 01:51:32 -0600

    chore(category): add category fixtures

---
+++ src/bikeshop/testing/categories.fixtures.ts
@@ -0,0 +1,11 @@
+import { Category } from '~bikeshop/category.entity';
+
+export const categories: Category[] = [
+  { id: 1n, name: 'Children Bicycles' },
+  { id: 2n, name: 'Comfort Bicycles' },
+  { id: 3n, name: 'Cruisers Bicycles' },
+  { id: 4n, name: 'Cyclocross Bicycles' },
+  { id: 5n, name: 'Electric Bikes' },
+  { id: 6n, name: 'Mountain Bikes' },
+  { id: 7n, name: 'Road Bikes' },
+];
