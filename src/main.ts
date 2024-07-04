bb926dd4879c360a322fe60108819e17b4d7f184
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-07-04 15:22:53 -0600

    chore: set port using a environmental variable

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 void bootstrap();
